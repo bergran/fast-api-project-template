19f3916826674b5438e17d5d02cf8b29cc7d8591
bergran <bergraan@gmail.com>
2019-06-22 10:26:07 +0200

    feat (tests): now creates and delete database on start and finish tests

--- {{cookiecutter.project_name}}/conftest.py
+++ {{cookiecutter.project_name}}/conftest.py
@@ -11,6 +11,19 @@ logger = logging.getLogger('fixture')
 logger.setLevel(logging.INFO)
 
 
+def pytest_sessionstart(session):
+    os.environ.setdefault('TEST_RUN', '1')
+    from main import app
+    _create_database(app.config.SQLALCHEMY_DATABASE_URI)
+    _create_database_connection(app)
+
+
+def pytest_sessionfinish(session):
+    os.environ.setdefault('TEST_RUN', '1')
+    from main import app
+    _delete_database(app.config.SQLALCHEMY_DATABASE_URI)
+
+
 def _create_database(dsn):
     database_name = dsn.split('/')[-1]
     logger.info('check database {}'.format(database_name))
@@ -26,23 +39,18 @@ def _create_database(dsn):
 
 @pytest.fixture(scope="class")
 def get_session_and_client_fixture(request):
+    from main import app
     # Set TEST_RUN environment to tell app that we are running under test environment to connect dummy test
     os.environ.setdefault('TEST_RUN', '1')
-    print('qwer', os.environ.get('TEST_RUN', 'a'))
 
-    from main import app
+    from core.db.base import Base
 
-    _create_database(app.config.SQLALCHEMY_DATABASE_URI)
-
-    session, Base = _create_database_connection(app)
+    engine = create_engine(app.config.SQLALCHEMY_DATABASE_URI)
+    session = sessionmaker(autocommit=False, autoflush=True, bind=engine)()
 
     request.cls.session = session
     request.cls.client = TestClient(app)
     request.cls.Base = Base
-
-    yield
-
-    _delete_database(app.config.SQLALCHEMY_DATABASE_URI)
 
 
 def _delete_database(dsn):
